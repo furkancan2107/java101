4c195476555d07f332dee80f0318941990a41b79
Furkan Can <101043567+furkancan2107@users.noreply.github.com>
2022-08-29 00:37:30 +0300

    www.patika.dev
     Ödev
      Java döngüler ile tek bir sayı girilene kadar
       kullanıcıdan girişleri kabul eden ve girilen değerlerden
        çift ve 4'ün katları olan sayıları toplayıp 
        ekrana basan programı yazıyoruz.
     */

---
+++ java101/src/java101/algoritma_sorusu_dongüler.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/*
+ * Java döngüler ile negatif bir değer girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden tek sayıları toplayıp ekrana basan programı yazıyoruz.
+
+  Ödev
+  Java döngüler ile tek bir sayı girilene kadar
+   kullanıcıdan girişleri kabul eden ve girilen değerlerden
+    çift ve 4'ün katları olan sayıları toplayıp 
+    ekrana basan programı yazıyoruz.
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        try (Scanner scan = new Scanner(System.in)) {
+            boolean sayiPozitifMi = true;
+            int toplam = 0;
+            while (sayiPozitifMi) {
+
+                System.out.println("Sayi girin: ");
+                int sayi = scan.nextInt();
+                if (sayi % 2 == 0 && sayi % 4 == 0) {
+                    toplam = toplam + sayi;
+                }
+
+                if (sayi < 0) {
+                    sayiPozitifMi = false;
+                }
+
+            }
+            System.out.println("Toplam: " + toplam);
+        }
+    }
+}
