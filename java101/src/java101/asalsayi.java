ab1fdc94c18318df9210d06fa1ca1b92981669e7
Furkan Can <101043567+furkancan2107@users.noreply.github.com>
2022-08-29 12:50:58 +0300

    www.patika.dev

---
+++ java101/src/java101/asalsayi.java
@@ -0,0 +1,22 @@
+//import java.util.Scanner;
+
+/*Asal sayi bulma 1-100 arasinda */
+public class App {
+    public static void main(String[] args) throws Exception {
+        int sayac = 0;
+        for (int sayi = 2; sayi <= 100; sayi++) {
+            int kontrol = 0;
+            for (int i = 2; i < sayi; i++) {
+                if (sayi % i == 0) {
+                    kontrol = 1;
+                    break;
+                }
+            }
+            if (kontrol == 0) {
+                System.out.println(sayi);
+                sayac++;
+            }
+        }
+
+    }
+}
