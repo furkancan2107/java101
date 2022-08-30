756f979e8b2d9edb1e1c2ef44687cf895210bee4
Furkan Can <101043567+furkancan2107@users.noreply.github.com>
2022-08-31 02:55:33 +0300

    Update recursiveilefibonacci.java

--- java101/src/java101/recursiveilefibonacci.java
+++ java101/src/java101/recursiveilefibonacci.java
@@ -1,36 +1,27 @@
 import java.util.Scanner;
 
-//import java.util.Scanner;
 
-/*Java ile girilen sayının harmonik serisini bulan program yazacağız. */
+
 public class App {
+
+    static int sonuc(int sayi) {
+        if (sayi == 1)
+            return 1;
+        if (sayi == 2)
+            return 1;
+        return sonuc(sayi - 1) + sonuc(sayi - 2);
+    }
 
     public static void main(String[] args) throws Exception {
 
-        try (Scanner scan = new Scanner(System.in)) {
-            System.out.print("Fibonacci dizisinin kaç elemanlı olacagini gir: ");
-            int d = scan.nextInt();
-            fibonnacci(d);
-        }
-    }
-
-    static void fibonnacci(int d) {
-        int a, b, c;
-        a = 1;
-        b = 1;
-        System.out.println(a);
-        System.out.println(b);
-        int i = 0;
-
-        while (i < d - 2) {
-            c = a + b;
-            a = b;
-            b = c;
-            System.out.println(c);
-            i++;
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("sayi gir: ");
+        int sayi = scan.nextInt();
+        for (int i = 1; i <= sayi; i++) {
+            System.out.println(sonuc(i));
         }
 
-    }
+        scan.close();
 
+    }
 }
