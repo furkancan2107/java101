2944c48310c10084a687c9b996fa628d1d590164
Furkan Can <101043567+furkancan2107@users.noreply.github.com>
2022-12-31 21:23:09 +0300

    ce

--- java101/src/java101/labirentOyunu.java
+++ java101/src/java101/labirentOyunu.java
@@ -1,52 +1,52 @@
 
-public class finalSoru {
-  // oyun matrisin[0][0] indexinden başlayıp birlerin olduğu yerlerden giderek 9 a ulaşması
-    public static void run(int col, int row, String[][] cozum, int[][] labirent1) {
-        while (true) {
-            if (col < 0 || row < 0 || col > 19 || row > 19) {
-                break;
-            }
-            cozum[col][row] = "1";
-            if (labirent1[col][row] == 0) {
-                System.out.println("labirent cozülemedi");
-                break;
-            }
+import java.util.Scanner;
 
-            if (labirent1[col][row] == 9) {
-                cozum[col][row] = "9";
-                System.out.println("Labirent cozüldü");
+public class Main {
+  //matrisin ilk indexinden başlayıp birleriin olduğu yoldan ilerleyerek 9 a ulaşmasını sağlayan oyun
+    static Scanner scan = new Scanner(System.in);
+
+    static void run(int col, int row, String[][] cozum, int[][] labirent1) {
+        while (true) {
+
+            if (labirent1[col][row] == 0) {
+                System.out.println("Labirent çözülemedi ");
                 break;
             }
             if (labirent1[col][row] == 1) {
-                // asagısi sagi ve solunu kontrol edecek
-                if (row > 0 && col < 19 && labirent1[col + 1][row] == 1) {
-
-                    col++;
-                } else if (row > 0 && row < 19 && labirent1[col][row + 1] == 1) {
-
-                    row++;
-                } else if (row > 0 && labirent1[col][row - 1] == 1) {
-
-                    row--;
-                } else {
-                    continue;
-                }
-
-                // asağısi ile sağını kontrol edecek
-                if (row == 0 && labirent1[col + 1][row] == 1) {
-                    col++;
-
-                } else if (row == 0 && labirent1[col][row + 1] == 1) {
-                    row++;
-
-                } else {
-                    continue;
-                }
-
+                cozum[col][row] = "1";
             }
-            // break;
-        }
+            if (labirent1[col][row] == 9) {
+                cozum[col][row] = "9";
+                System.out.println("Labirent çözüldü");
+                break;
+            }
+            System.out.println("1-sagaGit \n2-Sola git \n3-Asagi in");
+            System.out.println("Secim yapin: ");
+            int secim = scan.nextInt();
+            switch (secim) {
 
+                case 1:
+                    if (row < 19) {
+                        row++;
+                    }
+                    break;
+                case 2:
+                    if (row > 0) {
+                        row--;
+                    }
+                    break;
+                case 3:
+                    if (col < 19) {
+                        col++;
+                        break;
+                    }
+                default:
+                    System.out.println("Lütfen 1 ile 3 arasinda secim yapin");
+                    break;
+            }
+            System.out.println("col: " + col + "row: " + row);
+
+        }
     }
 
     public static void main(String[] args) {
@@ -72,24 +72,41 @@ public class finalSoru {
                 { 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
                 { 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 9, 0, 0, 1, 0, 0 },
                 { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-        };
-        String[][] czm = new String[20][20];
-        int i = 0;
-        int j = 0;
-        run(i, j, czm, labirent);
 
+        };
+
+        System.out.print("Satir gir: ");
+        int satir = scan.nextInt();
+        System.out.print("Sütün gir: ");
+        int sutun = scan.nextInt();
+        if (satir != 0 && sutun != 0) {
+            System.out.println("0 0 gir ilk degeri");
+
+        }
+        String[][] czm = new String[20][20];
+
+        for (int[] s : labirent) {
+            for (int z : s) {
+                System.out.printf("%4d", z);
+            }
+            System.out.println();
+
+        }
+        run(satir, sutun, czm, labirent);
         for (String[] a : czm) {
             for (String b : a) {
 
                 if (b == null) {
                     System.out.print(" ");
                 } else {
-                    System.out.print(b);
+                    System.out.printf(b);
                 }
 
             }
             System.out.println();
         }
+
     }
 
 }
+
